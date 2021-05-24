1afd984c18212a346c7aa4b63b77e344411906b4
Harsha Chaudhary <harshac@vmware.com>
2021-05-24 17:01:47 +0530

    Isogram: Removes nested loops

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -5,20 +5,12 @@ import "strings"
 //IsIsogram determines if given string is an isogram
 //It ignores strings with multiple spaces and hyphens
 func IsIsogram(input string) bool {
-	runes := []rune(strings.ToLower(input))
-	for i, r := range runes {
-		if exists(r, runes[i+1:]) {
+	var runeMap = make(map[rune]bool)
+	for _, r := range []rune(strings.ToLower(input)) {
+		if _, ok := runeMap[r]; ok && r != ' ' && r != '-' {
 			return false
 		}
+		runeMap[r] = true
 	}
 	return true
-}
-
-func exists(ir rune, runes []rune) bool {
-	for _, r := range runes {
-		if r == ir && !(r == '-' || r == ' ') {
-			return true
-		}
-	}
-	return false
 }
