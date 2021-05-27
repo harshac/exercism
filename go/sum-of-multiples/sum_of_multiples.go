a223aa5125973c2a47b1b65f4cff2022819f7123
Harsha Chaudhary <harshac@vmware.com>
2021-05-27 22:12:09 +0530

    Sum of multiples: Refactor to not use map

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -3,22 +3,13 @@ package summultiples
 //SumMultiples calculates the sum of all multiples of divisors within the limit
 func SumMultiples(limit int, divisors ...int) int {
 	var sum int
-	for k, _ := range multiples(limit, divisors...) {
-		sum += k
-	}
-	return sum
-}
-
-func multiples(limit int, divisors ...int) map[int]bool {
-	var multiples = map[int]bool{}
-	for _, divisor := range divisors {
-		multiple := divisor
-		multiplier := 2
-		for multiple > 0 && multiple < limit {
-			multiples[multiple] = true
-			multiple = divisor * multiplier
-			multiplier++
+	for i := 1; i < limit; i++ {
+		for _, divisor := range divisors {
+			if divisor > 0 && i%divisor == 0 {
+				sum += i
+				break
+			}
 		}
 	}
-	return multiples
+	return sum
 }
