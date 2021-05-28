d622e83588176212eeb740a705891cd8ae36f12f
Harsha Chaudhary <harshac@vmware.com>
2021-05-28 18:03:44 +0530

    Grains: Calculates number of grains on square of chess board

---
+++ go/grains/grains.go
@@ -0,0 +1,23 @@
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+//Square calculates the total number of grains on given square
+func Square(input int) (uint64, error) {
+	if input <= 0 || input > 64 {
+		return 0, errors.New("invalid input")
+	}
+	return grainsOnSquare(input), nil
+}
+
+//Square calculates the total number of grains on chessboard
+func Total() uint64 {
+	return (grainsOnSquare(64) * 2) - 1
+}
+
+func grainsOnSquare(input int) uint64 {
+	return uint64(math.Pow(float64(2), float64(input-1)))
+}
