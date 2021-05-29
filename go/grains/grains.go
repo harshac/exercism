3f71fd944f115decda75cf1da52b6af10b7f22a6
Harsha Chaudhary <harshac@vmware.com>
2021-05-29 09:54:13 +0530

    Grains: Uses bitwise operator instead of math lib

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,23 +1,22 @@
 package grains
 
-import (
-	"errors"
-	"math"
-)
+import "errors"
+
+const totalChessSquares = 64
 
 //Square calculates the total number of grains on given square
 func Square(input int) (uint64, error) {
-	if input <= 0 || input > 64 {
+	if input <= 0 || input > totalChessSquares {
 		return 0, errors.New("invalid input")
 	}
-	return grainsOnSquare(input), nil
+	return countOnSquare(input), nil
 }
 
 //Total calculates the total number of grains on chessboard
 func Total() uint64 {
-	return (grainsOnSquare(64) * 2) - 1
+	return (countOnSquare(totalChessSquares + 1)) - 1
 }
 
-func grainsOnSquare(input int) uint64 {
-	return uint64(math.Pow(float64(2), float64(input-1)))
+func countOnSquare(input int) uint64 {
+	return 1 << (input - 1)
 }
