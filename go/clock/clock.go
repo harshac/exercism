dd4f0a42f49caacb2acb1ab3fd3fd3252d3e90a8
Harsha Chaudhary <harshac@vmware.com>
2021-05-29 20:05:08 +0530

    Clock: Simplifies clock by representing in mins

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -7,42 +7,32 @@ const (
 	hoursInADay  = 24
 )
 
-//Clock represents time without date
+//Clock represents minutes
 type Clock struct {
-	hours int
-	mins  int
+	mins int
 }
 
 //String formats the time of the Clock
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hours, c.mins)
+	return fmt.Sprintf("%02d:%02d", c.mins/minsInAnHour, c.mins%minsInAnHour)
 }
 
 //Add adds given mins to the Clock
 func (c Clock) Add(mins int) Clock {
-	return New(c.hours, c.mins+mins)
+	return New(0, c.mins+mins)
 }
 
 //Subtract deducts given mins from the Clock
 func (c Clock) Subtract(mins int) Clock {
-	return New(c.hours, c.mins-mins)
+	return New(0, c.mins-mins)
 }
 
 //New creates a new Clock without date
 func New(hours int, mins int) Clock {
-	if mins < 0 {
-		hours += (mins / minsInAnHour) - 1
-		mins = minsInAnHour + (mins % minsInAnHour)
+	m := mins + hours*minsInAnHour
+	m %= hoursInADay * minsInAnHour
+	if m < 0 {
+		m += hoursInADay * minsInAnHour
 	}
-	if hours < 0 {
-		hours = hoursInADay + (hours % hoursInADay)
-	}
-	if mins >= minsInAnHour {
-		hours += mins / minsInAnHour
-		mins %= minsInAnHour
-	}
-	if hours >= hoursInADay {
-		hours %= hoursInADay
-	}
-	return Clock{hours: hours, mins: mins}
+	return Clock{mins: m}
 }
